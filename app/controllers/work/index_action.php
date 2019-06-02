b9344714cfd29f6174daf0413acc90313802210a
Mamoru Misono <mamo@sonosoft.com>
2019-06-02 22:01:03 +0900

    WORK/PAGE

--- app/controllers/work/index_action.php
+++ app/controllers/work/index_action.php
@@ -14,7 +14,7 @@ class WorkIndexAction extends Controller {
    */
   public function action(){
     /**/
-    $this->useModel('User', 'Project', 'ProjectUser', 'Visit', 'Page');
+    $this->useModel('User', 'Project', 'ProjectUser', 'ProjectDiary', 'Visit', 'Page');
     
     /* 検索条件初期化 */
     $this->app->removeSession('work_data');
