b1458a35c28953cedb246d4f07189b26c13675f3
Mamoru Misono <mamo@sonosoft.com>
2019-06-02 22:41:45 +0900

    answer model

---
+++ app/models/answer_model.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * [Elnath PHP Web Application Framework]
+ * Copyright (c) 2013 SONOSOFT Inc., All rights reserved.
+ *
+ * answer_model.php
+ */
+
+
+class AnswerModelFactory extends ModelFactory {
+  /*
+   * プロパティ
+   */
+  protected $model = 'Answer';
+  protected $table = 'answer';
+}
+
+class AnswerModel extends Model {
+}
