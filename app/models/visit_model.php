48b8292bd3d5acf5744c998f4f635d38e8ef6b4b
Mamoru Misono <mamo@sonosoft.com>
2019-06-02 20:29:27 +0900

    VisitModel

---
+++ app/models/visit_model.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * [Elnath PHP Web Application Framework]
+ * Copyright (c) 2013 SONOSOFT Inc., All rights reserved.
+ *
+ * visit_model.php
+ */
+
+
+class VisitModelFactory extends ModelFactory {
+  /*
+   * プロパティ
+   */
+  protected $model = 'Visit';
+  protected $table = 'visit';
+}
+
+class VisitModel extends Model {
+}
