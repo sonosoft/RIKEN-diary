d137871568e578d6f16a559951bcabe7d15698ea
Mamoru Misono <mamo@sonosoft.com>
2019-06-02 18:11:48 +0900

    project

---
+++ app/validators/user_search_validator.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * [Elnath PHP Web Application Framework]
+ * Copyright (c) 2013 SONOSOFT Inc., All rights reserved.
+ *
+ * user_search_validator.php
+ */
+
+
+class UserSearchValidator extends Validator {
+  /*
+   * 検証
+   */
+  protected function validateData(){
+    /**/
+    $this->validatePage('page');
+    /**/
+    $this->validateTextList('text');
+    $this->ifEmpty('from', null)->toDate('from');
+    $this->ifEmpty('to', null)->toDate('to');
+    $this->ifEmpty('order', 'i-d');
+    /**/
+    $this->toBoolean('all');
+    /**/
+    $this->toBoolean('download');
+    /**/
+    $this->clearError();
+  }
+}
