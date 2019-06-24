7702c83d1c9a40582c037cf90d6177f3859df22d
Mamoru Misono <mamo@sonosoft.com>
2019-06-24 21:48:06 +0900

    PAGE

--- app/models/diary_model.php
+++ app/models/diary_model.php
@@ -59,6 +59,7 @@ class DiaryModel extends Model {
 	$path = sprintf('%s/%02x', $path, substr($hex, $i, 2));
       }
       $path .= '.xml';
+      var_dump($path);
       return $this->app->projectFile($path);
     }else if(strcmp($name, 'schedule_tos') == 0){
       return sprintf(
