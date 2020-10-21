65f75f2c2ed6a92f064cb50e26980d58732e146e
Mamoru Misono <mamo@sonosoft.com>
2020-10-21 12:18:56 +0900

    PHPMailer

--- app/scripts/script-test-mail.php
+++ app/scripts/script-test-mail.php
@@ -1,5 +1,10 @@
 <?php
 
+include_once(__DIR__.'/vendor/autoload.php');
+
+$mail = new PHPMailer();
+var_dump($mail);
+
 $text = 'しましたか？【日誌|午前|摂取|2020/10/16】【】です。';
 
 if(preg_match('/【日誌\|(起床時|午前|午後|就寝時)\|(入力|摂取)\|([\/0-9]+)】/', $text, $matches)){
